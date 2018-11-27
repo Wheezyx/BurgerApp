e6de809e1d153bc532356d8a5af9d769e467edd8
Ernest <mglernest@gmail.com>
2018-11-27 13:59:12 +0100

    order done

--- src/main/java/burgerapp/components/HomeController.java
+++ src/main/java/burgerapp/components/HomeController.java
@@ -2,6 +2,8 @@ package burgerapp.components;
 
 import burgerapp.components.burger.Burger;
 import burgerapp.components.burger.BurgerService;
+import burgerapp.components.user.User;
+import burgerapp.components.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,21 +14,25 @@ import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
-public class HomeController
-{
+public class HomeController {
     private BurgerService burgerService;
-    
+    private UserService userService;
+
     @GetMapping("/")
-    public String home(Model model)
-    {
+    public String home(Model model) {
         Optional<List<Burger>> burgers = burgerService.getAll();
         burgers.ifPresent(burs -> model.addAttribute("burgers", burs));
         return "index2";
     }
-    
+
     @GetMapping("/login")
-    public String login()
-    {
+    public String login() {
         return "login";
+    }
+
+    @GetMapping("add-admin")
+    public String add() {
+        this.userService.addWithDefaultRole(new User("admin", "admin"));
+        return "index2";
     }
 }
